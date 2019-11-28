6961e3d61e1aa442901a75a6f43c0e8a4626edf9
Nikita Polozov <n.polozov@tapston.com>
2019-11-28 16:13:54 +0300

    update filters interval

--- src/functions/filter-interval.js
+++ src/functions/filter-interval.js
@@ -1,20 +1,49 @@
 const getValueFromObj = require('./get-value-from-obj');
 
+const convert = (data, input) => {
+  const type = typeof input;
+  let output = data;
+  switch (type) {
+    case 'number':
+      if (!isNaN(+data)) {
+        output = Number(data);
+      };
+      break;
+    case 'string':
+      output = String(data);
+      break;
+    case 'boolean':
+      output = Boolean(data);
+      break;
+    case 'object':
+      if (!isNaN(Date.parse(data))) {
+        output = new Date(data);
+      } else {
+        output = data;
+      };
+      break;
+    default:
+      output = data;
+      break;
+  };
+  return output;
+};
+
 module.exports = (collection, key, search) => {
   return collection.indexes[key].filter(item => {
     if (collection.documents[item]) {
       const value = getValueFromObj(collection.documents[item], key);
       let output = true;
-      if (search.$gte && value < search.$gte) {
+      if (search.$gte && convert(value, search.$gte) < search.$gte) {
         output = false;
       };
-      if (search.$gt && value <= search.$gt) {
+      if (search.$gt && convert(value, search.$gt) <= search.$gt) {
         output = false;
       };
-      if (search.$lte && value > search.$lte) {
+      if (search.$lte && convert(value, search.$lte) > search.$lte) {
         output = false;
       };
-      if (search.$lt && value >= search.$lt) {
+      if (search.$lt && convert(value, search.$lt) >= search.$lt) {
         output = false;
       };
       return output;
